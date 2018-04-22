66286780a6e44c803ba0c3bf98d3692ed4920328
andyreesecups <andyreesecups@yahoo.com>
2018-04-22 13:57:03 -0500

    added slider functionality, and autocomplete

--- js/script.js
+++ js/script.js
@@ -1,3 +1,29 @@
 // Sidenav Hamburger Menu Functionality
 const sideNav = document.querySelector('.sidenav');
 M.Sidenav.init(sideNav, {});
+
+// Slider
+const slider = document.querySelector('.slider');
+M.Slider.init(slider, {
+    indicators: false,
+    height: 500,
+    transition: 500,
+    interval: 6000
+});
+
+// Autocomplete
+const ac = document.querySelector('.autocomplete');
+M.Autocomplete.init(ac, {
+    data: {
+        "Aruba": null,
+        "Italy": null,
+        "Cancun, Mexico": null,
+        "Brazil": null,
+        "Rome": null,
+        "UK": null,
+        "Paris": null,
+        "Africa": null,
+        "Florida": null,
+        "Jamaica": null,
+    }
+});
